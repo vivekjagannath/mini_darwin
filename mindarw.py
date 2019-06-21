f087e91f96f829e2607e2a2f2105d6fdd5f8f16e
vivekjagannath <vivekj1203@gmail.com>
2019-06-21 17:37:30 +0530

    updates to stop infinite loops.

--- mindarw.py
+++ mindarw.py
@@ -1,20 +1,22 @@
 
 import serial
 from time import sleep
-roboser = serial.Serial('/dev/ttyACM5', 115200) # Establish the connection on a specific port
+roboser = serial.Serial('/dev/ttyACM1', 1000000) # Establish the connection on a specific port
 con='1'
 control=5
 while 1:
     control=input("enter 1 for pushup and 2 for Walk:")
     if int(control)==1:
-        con=str(0)
-        tim=.1
-    elif int(control) == 2:
         con=str(1)
-        tim=20
+    elif int(control) == 2:
+        con=str(2)
+        roboser.write(bytes(con))
+        iss = input("enter 1 to stop:")
+        if int(iss) == 1:
+            con=str(1)
+            print iss
     print control
-    sleep(1)
+
     roboser.write(bytes(con)) # Convert the decimal number to ASCII then send it to the Arduino 
-    yo = str(roboser.readline()) # Read the newest output from the Arduino
-    print (yo[2:len(yo)-5])
-    sleep(tim) # Delay for one tenth of a second
+    #sleep(1)
+    sleep(.1) # Delay for one tenth of a second
