4dfe58b7d9b7eaa209546ab501e2e055708aa82d
vivekjagannath <vivekj1203@gmail.com>
2019-06-22 16:39:56 +0530

    added right and left commands.

--- mindarw.py
+++ mindarw.py
@@ -1,11 +1,13 @@
 
 import serial
 from time import sleep
-roboser = serial.Serial('/dev/ttyACM1', 1000000) # Establish the connection on a specific port
+
+roboser = serial.Serial('/dev/ttyACM2', 1000000)
 con='1'
 control=5
-while 1:
-    control=input("enter 1 for pushup and 2 for Walk:")
+
+while True:
+    control=input("enter 1 for pushup, 2 for Walk, 3 for right and 4 for left:")
     if int(control)==1:
         con=str(1)
     elif int(control) == 2:
@@ -15,8 +17,21 @@ while 1:
         if int(iss) == 1:
             con=str(1)
             print iss
-    print control
+    elif int(control) == 3:
+        con = str(3)
+        roboser.write(bytes(con))
+        iss = input("enter 1 to stop:")
+        if int(iss) == 1:
+            con=str(1)
+            print iss
+    elif int(control) == 4:
+        con = str(4)
+        roboser.write(bytes(con))
+        iss = input("enter 1 to stop:")
+        if int(iss) == 1:
+            con=str(1)
+            print iss
 
-    roboser.write(bytes(con)) # Convert the decimal number to ASCII then send it to the Arduino 
-    #sleep(1)
-    sleep(.1) # Delay for one tenth of a second
+    print control
+    roboser.write(bytes(con))  
+    sleep(.1) 
